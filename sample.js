42ca0eb24503b222d30631c8984ac1a46f737eb9
deepakjoelr <deepakjoel@gmail.com>
2026-02-04 12:02:05 +0530

    Add sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,4 @@
-let s="Hello World"
-let words=s.split(" ")
-let lastword=words[words.length-1]
-console.log("Length is:",lastword.length)
+let s="Hello World";
+let words=s.split(" ");
+let lastword=words[words.length-1];
+console.log("Length is:",lastword.length);
